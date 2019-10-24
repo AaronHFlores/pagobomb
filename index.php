2c81a086af1ff525462d697be5438b7f28290605
firestriker21 <55005942+firestriker21@users.noreply.github.com>
2019-10-23 23:56:27 -0500

    a

--- index.php
+++ index.php
@@ -1,27 +1,3 @@
 <?php
-  session_start();
-  if(isset($_SESSION['contador']))
-  {
-    $_SESSION['contador'] = $_SESSION['contador'] + 1;
-    $mensaje = 'Número de visitas: ' . $_SESSION['contador'];
-  }
-  else
-  {
-    $_SESSION['contador'] = 1;
-    $mensaje = 'Bienvenido a nuestra página web';
-  }
+  require_once("index.html")
 ?>
-<?xml version="1.0" encoding="iso-8859-1"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Prueba de cookie</title>
-</head>
-<body>
-<p>
-<?php echo $mensaje; ?>
-</p>
-</body>
-</html>
